517c645ba579e4d036a126be6bd31d0413cc1eed
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-28 22:48:00 +0530

    Create Print Square Number Pattern.py

---
+++ Print Square Number Pattern.py
@@ -0,0 +1,10 @@
+# Python Program to Print Square Number Pattern
+ 
+side = int(input("Please Enter any Side of a Square  : "))
+
+print("Square Number Pattern") 
+
+for i in range(side):
+    for i in range(side):
+        print('1', end = '  ')
+    print()
