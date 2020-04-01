f29851badb2c5daa07967800666792404ab27f5d
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-01 11:31:40 +0530

    Create Program to Print F.loyd’s Triangle.py

---
+++ Program to Print F.loyd’s Triangle.py
@@ -0,0 +1,31 @@
+# Python Program to Print Floyd's Triangle
+
+rows = int(input("Please Enter the total Number of Rows  : "))
+number = 1
+
+print("Floyd's Triangle") 
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):        
+        print(number, end = '  ')
+        number = number + 1
+    print()
+    
+   
+   ******************************************************************************************
+    By the help of While loop.
+
+    rows = int(input("Please Enter the total Number of Rows  : "))
+    number = 1
+
+    print("Floyd's Triangle")
+    i = 1
+    while(i <= rows):
+          j = 1
+          while(j <= i):        
+          print(number, end = '  ')
+          number = number + 1
+          j = j + 1
+          i = i + 1
+          print()
+
+          
