2f5000d53e2d82b241079e2ef9c0e3914393cfb2
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-04 00:47:36 +0530

    Create find Volume and Surface Area of a Cone using functions.py

---
+++ find Volume and Surface Area of a Cone using functions.py
@@ -0,0 +1,23 @@
+# Python Program to find Volume and Surface Area of a Cone using functions
+
+import math
+
+def Vo_Sa_Cone(radius, height):
+    # Calculate Length of a Slide (Slant)
+    l = math.sqrt(radius * radius + height * height)
+
+    # Calculate the Surface Area
+    SA = math.pi * radius * (radius + l)
+
+    # Calculate the Volume
+    Volume = (1.0/3) * math.pi * radius * radius * height
+
+    # Calculate the Lateral Surface Area
+    LSA = math.pi * radius  * l
+
+    print("\n Length of a Side (Slant)of a Cone = %.2f" %l)
+    print(" The Surface Area of a Cone = %.2f " %SA)
+    print(" The Volume of a Cone = %.2f" %Volume)
+    print(" The Lateral Surface Area of a Cone = %.2f " %LSA)
+
+Vo_Sa_Cone(6,10)
