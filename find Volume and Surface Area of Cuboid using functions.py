9753bd8c7a7348e0738744285d5e3cfdbd7f1c8a
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-04 00:52:30 +0530

    Create find Volume and Surface Area of Cuboid using functions.py

---
+++ find Volume and Surface Area of Cuboid using functions.py
@@ -0,0 +1,17 @@
+# Python Program to find Volume and Surface Area of a Cuboid using Functions
+
+def Vo_Sa_Cuboid(length, width, height):
+    # Calculate the Surface Area
+    SA = 2 * (length * width + length * height + width * height)
+
+    # Calculate the Volume
+    Volume = length * width * height
+
+    # Calculate the Lateral Surface Area
+    LSA = 2 * height * (length + width)
+
+    print("\n The Surface Area of a Cuboid = %.2f " %SA)
+    print(" The Volume of a Cuboid = %.2f" %Volume)
+    print(" The Lateral Surface Area of a Cuboid = %.2f " %LSA)
+
+Vo_Sa_Cuboid(9, 4, 6)
