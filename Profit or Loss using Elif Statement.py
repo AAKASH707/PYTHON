e6c6aa32a75b2bd1566961ab43d34243c46c1a14
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-05 22:28:34 +0530

    Update Profit or Loss using Elif Statement.py

--- Profit or Loss using Elif Statement.py
+++ Profit or Loss using Elif Statement.py
@@ -11,3 +11,19 @@ elif(sale_amount > actual_cost):
     print("Total Profit = {0}".format(amount))
 else:
     print("No Profit No Loss!!!")
+  *******************************************************************************************
+  
+                        // Profit or Loss using Arithmetic Operator.
+   # Python Program to Calculate Profit or Loss
+ 
+actual_cost = float(input(" Please Enter the Actual Product Price: "))
+sale_amount = float(input(" Please Enter the Sales Amount: "))
+ 
+if(actual_cost - sale_amount > 0):
+    amount = actual_cost - sale_amount
+    print("Total Loss Amount = {0}".format(amount))
+elif(sale_amount - actual_cost > 0):
+    amount = sale_amount - actual_cost
+    print("Total Profit = {0}".format(amount))
+else:
+    print("No Profit No Loss!!!")
