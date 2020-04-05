72b72c8a475966b49089c3e8761c1ee06a5f2203
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-05 22:34:53 +0530

    Update Fibonacci Series.py
    added using recursion

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -39,3 +39,24 @@ for Num in range(0, Number):
                       First_Value = Second_Value
                       Second_Value = Next
            print(Next)
+******************************************************************************************************************************************
+// Using Recursion
+# Python Fibonacci series Program using Recursion
+
+# Recursive Function Beginning
+def Fibonacci_series(Number):
+           if(Number == 0):
+                      return 0
+           elif(Number == 1):
+                      return 1
+           else:
+                      return (Fibonacci_series(Number - 2)+ Fibonacci_series(Number - 1))
+
+# End of the Function
+
+# Fibonacci series will start at 0 and travel upto below number
+Number = int(input("\nPlease Enter the Range Number: "))
+
+# Find & Displaying Fibonacci series
+for Num in range(0, Number):
+           print(Fibonacci_series(Num))
