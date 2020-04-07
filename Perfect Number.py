20e0d124c86c05541d06413493ebdda938e76ded
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-07 19:20:29 +0530

    Update Perfect Number.py

--- Perfect Number.py
+++ Perfect Number.py
@@ -41,3 +41,23 @@ if (Number == Perfect_Number(Number)):
     print("\n %d is a Perfect Number" %Number)
 else:
     print("\n %d is not a Perfect Number" %Number)
+*****************************************************************************************************************************
+                                           // Program to Find Perfect Number between 1 to 1000
+    # Python Program to find Perfect Number between 1 to 1000
+
+# Taking input from the user
+Minimum = int(input("Please Enter any Minimum Value: "))
+Maximum = int(input("Please Enter any Maximum Value: "))
+
+# initialise sum
+
+
+# Checking the Perfect Number
+for Number in range(Minimum, Maximum - 1):
+    Sum = 0
+    for n in range(1, Number - 1):
+        if(Number % n == 0):
+            Sum = Sum + n       
+    # display the result
+    if(Sum == Number):
+        print(" %d " %Number)
