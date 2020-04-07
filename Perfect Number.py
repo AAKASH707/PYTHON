e7cdc61d8097f4f312b6d12d57e5f6e0fe11d7d1
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-07 19:19:10 +0530

    Update Perfect Number.py

--- Perfect Number.py
+++ Perfect Number.py
@@ -24,3 +24,20 @@ if (Sum == Number):
     print(" %d is a Perfect Number" %Number)
 else:
     print(" %d is not the Perfect Number" %Number)                                                    
+*******************************************************************************************************************************
+                                            // Perfect Number using Function
+    # Python Program to find Perfect Number using Functions
+
+def Perfect_Number(Number):
+    Sum = 0
+    for i in range(1, Number):
+        if(Number % i == 0):
+            Sum = Sum + i
+    return Sum        
+
+# Taking input from the user
+Number = int(input("Please Enter any number: "))
+if (Number == Perfect_Number(Number)):
+    print("\n %d is a Perfect Number" %Number)
+else:
+    print("\n %d is not a Perfect Number" %Number)
