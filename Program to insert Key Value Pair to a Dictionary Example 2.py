088b343186fbc42bcaf3ab50e790ae96fdb345fd
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-13 20:25:49 +0530

    Create Program to insert Key Value Pair to a Dictionary Example 2.py

---
+++ Program to insert Key Value Pair to a Dictionary Example 2.py
@@ -0,0 +1,10 @@
+# Python Program to Add Key-Value Pair to a Dictionary
+
+key = input("Please enter the Key : ")
+value = input("Please enter the Value : ")
+
+myDict = {}
+
+# Add Key-Value Pair to a Dictionary in Python
+myDict[key] = value
+print("\nUpdated Dictionary = ", myDict)
