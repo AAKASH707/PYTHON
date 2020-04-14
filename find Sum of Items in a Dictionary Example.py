9da2df82fa89274bc484cc970965d8a90fdb076e
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-14 20:10:22 +0530

    Update find Sum of Items in a Dictionary Example.py

--- find Sum of Items in a Dictionary Example.py
+++ find Sum of Items in a Dictionary Example.py
@@ -5,3 +5,15 @@ print("Dictionary: ", myDict)
 
 # Print Values using get
 print("\nSum of Values: ", sum(myDict.values()))
+*******************************************************************************************************
+                                 # Python Program to find Sum of Items in a Dictionary Example 2
+
+myDict = {'x': 250, 'y':500, 'z':410}
+print("Dictionary: ", myDict)
+total = 0
+
+# Print Values using get
+for i in myDict.values():
+    total = total + i
+    
+print("\nThe Total Sum of Values : ", total)
