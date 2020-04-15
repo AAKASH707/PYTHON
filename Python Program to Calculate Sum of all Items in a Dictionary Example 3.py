a735ec1bec64f36d994393246fcb51e35e58594f
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-15 19:42:43 +0530

    Create Python Program to Calculate Sum of all Items in a Dictionary Example 3.py

---
+++ Python Program to Calculate Sum of all Items in a Dictionary Example 3.py
@@ -0,0 +1,11 @@
+# Python Program to find Sum of Items in a Dictionary
+
+myDict = {'x': 250, 'y':500, 'z':410}
+print("Dictionary: ", myDict)
+total = 0
+
+# Print Values using get
+for i in myDict:
+    total = total + myDict[i]
+    
+print("\nThe Total Sum of Values : ", total)
