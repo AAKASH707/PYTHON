14dd233fc68eeae6809f45ab026ebfde651bdf83
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-16 12:35:37 +0530

    Update Count Words in a String using Dictionary Example.py

--- Count Words in a String using Dictionary Example.py
+++ Count Words in a String using Dictionary Example.py
@@ -8,3 +8,16 @@ frequency = [words.count(i) for i in words]
 
 myDict = dict(zip(words, frequency))
 print("Dictionary Items  :  ",  myDict)
+
+***********************************************************************************************
+# Python Program to Count words in a String using Dictionary 2
+
+string = input("Please enter any String : ")
+words = []
+
+words = string.split() # or string.lower().split()
+myDict = {}
+for key in words:
+    myDict[key] = words.count(key)
+
+print("Dictionary Items  :  ",  myDict)
