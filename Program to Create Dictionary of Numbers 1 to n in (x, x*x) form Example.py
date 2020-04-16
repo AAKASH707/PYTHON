b088a6f223c644ac10f9125d7ade6e6ac15b4370
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-17 00:03:32 +0530

    Update Program to Create Dictionary of Numbers 1 to n in (x, x*x) form Example.py

--- Program to Create Dictionary of Numbers 1 to n in (x, x*x) form Example.py
+++ Program to Create Dictionary of Numbers 1 to n in (x, x*x) form Example.py
@@ -7,3 +7,11 @@ for x in range(1, number + 1):
     myDict[x] = x * x
     
 print("\nDictionary = ", myDict)
+****************************************************************************************
+# Python Program to Create Dictionary of Numbers 1 to n in (x, x*x) form example 2
+
+number = int(input("Please enter the Maximum Number : "))
+
+myDict = {x:x * x for x in range(1, number + 1)}
+
+print("\nDictionary = ", myDict)
