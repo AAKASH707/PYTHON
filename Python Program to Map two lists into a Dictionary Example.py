e30b2c6a03a3eebf3f180e34bb3269a53230a5cd
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-23 19:56:45 +0530

    Update Python Program to Map two lists into a Dictionary Example.py

--- Python Program to Map two lists into a Dictionary Example.py
+++ Python Program to Map two lists into a Dictionary Example.py
@@ -5,3 +5,12 @@ values = ['John', 25, 'Developer']
 
 myDict = {k: v for k, v in zip(keys, values)}
 print("Dictionary Items  :  ",  myDict)
+
+********************************************************************************************
+# Python Program to Map two lists into a Dictionary 2
+
+keys = ['name', 'age', 'job']
+values = ['John', 25, 'Developer']
+
+myDict = dict(zip(keys, values))
+print("Dictionary Items  :  ",  myDict)
