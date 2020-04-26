7ef26718cc2b9d0c6e6af8f6c4d7b05dd3163e7e
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-26 20:31:33 +0530

    Update Python Program to Multiply All Items in a Dictionary Example 1.py

--- Python Program to Multiply All Items in a Dictionary Example 1.py
+++ Python Program to Multiply All Items in a Dictionary Example 1.py
@@ -9,3 +9,16 @@ for key in myDict:
     total = total * myDict[key]
     
 print("\nAfter Multiplying Items in this Dictionary: ", total)
+*************************************************************************************************
+# Python Program to Multiply All Items in a Dictionary 2
+
+
+myDict = {'x': 2, 'y':50, 'z':70}
+print("Dictionary: ", myDict)
+
+total = 1
+# Multiply Items
+for i in myDict.values():
+    total = total * i
+    
+print("\nAfter Multiplying Items in this Dictionary: ", total)
