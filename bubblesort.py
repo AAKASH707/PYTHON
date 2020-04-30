af52790cff8d0480998032d48eec726611a213a7
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-30 20:44:12 +0530

    Update bubblesort.py
    updated bubble sort using functions

--- bubblesort.py
+++ bubblesort.py
@@ -35,3 +35,21 @@ while(i < number -1):
     i = i + 1
 
 print("The Sorted List in Ascending Order : ", a)
+*************************************************************************************************************
+# Python program for Bubble Sort using functions
+def bubblesort(a, number):
+    for i in range(number -1):
+        for j in range(number - i - 1):
+            if(a[j] > a[j + 1]):
+                 temp = a[j]
+                 a[j] = a[j + 1]
+                 a[j + 1] = temp
+
+a = []
+number = int(input("Please Enter the Total Number of Elements : "))
+for i in range(number):
+    value = int(input("Please enter the %d Element of List1 : " %i))
+    a.append(value)
+
+bubblesort(a, number)
+print("The Sorted List in Ascending Order : ", a)
